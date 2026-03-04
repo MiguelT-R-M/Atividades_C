59f5b81b34d0b38ef7db05b28fb0abb105246de2
mhrs-cin <mhrs@cin.ufpe.br>
2026-03-04 20:47:48 -0300

    At_2_lista1 terminada!

--- Atividades_Listas/Lista_1/At_2.c
+++ Atividades_Listas/Lista_1/At_2.c
@@ -1,20 +1,34 @@
+//Usei IA apenas para encontrar as fórmulas usadas
+
 #include <stdio.h>
 #include <math.h>
 
 int main(){
-    unsigned int N, A, B, SOMA = 0;
+    unsigned int N, A, B, Soma_total, SOMAA, SOMAB, AM, BM, Soma_final, MMCAB, MMCABM, SOMMC;
 
     scanf("%u", &N);
     scanf("%u %u", &A, &B);
 
-    for(unsigned int C = 1; C<=10; C++){
-        if(C%A!=0 && C%B!=0){
-            SOMA+=C;
-        }
+    Soma_total = (N*(N+1))/2; // Formula de soma total de 1 até N
+    
+    if(A!=B){ // Define o MMC entre A e B, desta forma por sempre serem numeros primos
+        MMCAB = A*B; 
+    }else{
+        MMCAB = A;
     }
 
-    printf("%u\n", SOMA);
-    if(SOMA%2==0){
+    AM = N/A; // Quantidade de multiplos de A até N
+    BM = N/B; // Quantidade de multiplos de B até N
+    MMCABM = N/MMCAB; // Quantidade de multiplos entre o MMC de A e B até N
+
+    SOMAA = A*((AM*(AM+1))/2); // Soma dos multiplos de A até N
+    SOMAB = B*((BM*(BM+1))/2); // Soma dos multiplos de B até N
+    SOMMC = MMCAB*((MMCABM*(MMCABM+1))/2); // Soma dos multiplos do MMC entre A e B até N
+
+    Soma_final = Soma_total - (SOMAA + SOMAB - SOMMC); // Formula da soma final
+
+    printf("%u\n", Soma_final);
+    if(Soma_final%2==0){
         printf("Lá ele!!!");
     }else{
         printf("Opa xupenio AULAS...");
