b52cbe4a001a5aafde943762ac777a874f85ae7e
mhrs-cin <mhrs@cin.ufpe.br>
2026-03-09 11:31:40 -0300

    estudos

--- Estudos/teste.c
+++ Estudos/teste.c
@@ -1,55 +1,23 @@
 #include <stdio.h>
-#define FORI(n) for(int i = 0; i<n; i++)
-
-int maxCycleFunc(int M, int N){
-
-    int Max_cycle_size = 0;
-
-    for(int A = M; A<=N; A++){
-
-        int TEMPCYCLE = 1, TEMPVAR1 = A, TEMPVAR2 = 0;
-
-        while(TEMPVAR1!=1){
-
-            if(TEMPVAR1%2==0){
-
-                TEMPVAR2 = TEMPVAR1/2;
-                TEMPVAR1 = TEMPVAR2;
-                TEMPCYCLE+=1;
-
-            }else if(TEMPVAR1%2!=0){
-
-                TEMPVAR2 = (TEMPVAR1*3)+1;
-                TEMPVAR1 = TEMPVAR2;
-                TEMPCYCLE+=1;
-
-            }
-
-        }
-
-        if(TEMPCYCLE>Max_cycle_size){
-
-            Max_cycle_size = TEMPCYCLE;
-
-        }
-
-    }
-    return Max_cycle_size;
-}
+#include <math.h>
+#define arrayLength(X) (sizeof(X)/sizeof(X[0]))
+#define stringLength(X) (sizeof(X)/sizeof(X[0])-1)
 
 int main(){
 
-    int I, J;
+    int A, B, C;
+    scanf("%d\n%d\n%d", &A, &B, &C);
 
-    while (scanf("%d %d", &I, &J) != EOF){
+    int arr[3];
 
-        printf("\n");
+    if(A<=B && A<=C){arr[0]=A;if(B<=C){arr[1]=B;arr[2]=C;}else{arr[1]=C;arr[2]=B;}}
+    if(B<=A && B<=C){arr[0]=B;if(A<=C){arr[1]=A;arr[2]=C;}else{arr[1]=C;arr[2]=A;}}
+    if(C<=B && C<=A){arr[0]=C;if(B<=A){arr[1]=B;arr[2]=A;}else{arr[1]=A;arr[2]=B;}}
 
-        int Max_cycle_size;
-        Max_cycle_size = maxCycleFunc(I, J);
-        printf("%u %u %u", I, J, Max_cycle_size);
+    for(int i = 0; i<arrayLength(arr); i++){
+
+        printf("%d\n", arr[i]);
 
     }
 
-    return 0;
 }
