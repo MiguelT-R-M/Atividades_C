518429e1c2a253cbbca8e3624b221afbf4c8f2ea
mhrs-cin <mhrs@cin.ufpe.br>
2026-03-16 23:08:35 -0300

    ..

--- Atividades_Listas/Lista_2/at_3.c
+++ Atividades_Listas/Lista_2/at_3.c
@@ -1,72 +1,98 @@
+
+// O C?DIGO FOI CORRIGIDO POR IA
+
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 
-int main(void){
+int main() { // O C?DIGO FOI CORRIGIDO POR IA
+    long long num_xupenio, n_canais;
+    
+    // Lendo a entrada inicial
+    if (scanf("%lld %lld", &num_xupenio, &n_canais) != 2) return 0;
 
-    long long int VIP = 0, Num_inicial, Canal_num, Stop_condition = 1, VAL1, VAL2, Proximidade_val  = 0, Proximidade_canal = -1, Min_importante = -1, Antes_proximidade_val, Antes_proximidade_canal, Antes_min_importante, Canal_importante = -1;
+    long long melhor_canal = -1;
+    long long melhor_minuto = -1;
+    long long melhor_distancia = -1;
+    long long melhor_valor_sequencia = -1;
 
-    scanf("%lld %lld", &Num_inicial, &Canal_num);
+    for (int i = 1; i <= n_canais; i++) {
+        long long v1, v2;
+        scanf("%lld %lld", &v1, &v2);
 
-    for(int i = 1; i<=Canal_num; i++){
+        long long f_penultimo = v1;
+        long long f_ultimo = v2;
+        
+        // Vari?veis para guardar o melhor termo DESTE canal espec?fico
+        long long canal_melhor_dist = -1;
+        long long canal_melhor_minuto = -1;
+        long long canal_melhor_valor = -1;
 
-        long long int Soma_val = 0, Soma_superpreanterior = Num_inicial, Soma_preanterior = 0, Soma_anterior = 0, MIN = 0, Proximidade_V1 = 0, Proximidade_V2 = 0, Antes_PV1 = 0 , Antes_PV2 = 0;
-        scanf("%lld %lld", &VAL1,&VAL2);
-        Soma_preanterior = VAL1; Soma_anterior = VAL2;
+        // Vamos testar os termos da sequ?ncia um por um
+        for (int minuto = 1; ; minuto++) {
+            long long termo_atual;
 
-        while (Stop_condition){
-
-            MIN++;
-            Soma_val = Soma_preanterior+Soma_anterior;
-            Proximidade_V1 = llabs(Soma_preanterior-Num_inicial); Proximidade_V2 = llabs(Soma_anterior-Num_inicial);
-            if((Proximidade_V1 == 0 || Proximidade_V2 == 0) || ((MIN>1) && (Proximidade_V1>Antes_PV1 || Proximidade_V2>Antes_PV2))){
-                Antes_min_importante = MIN;
-                Stop_condition = 0;
-                if(Antes_PV1<Antes_PV2){
-                    Antes_proximidade_canal = Antes_PV1; Antes_proximidade_val = Soma_superpreanterior;
-                }else if(Antes_PV2<Antes_PV1){
-                    Antes_proximidade_canal = Antes_PV2; Antes_proximidade_val = Soma_preanterior;
-                }else if(Antes_PV2==Antes_PV1){
-                    Antes_proximidade_canal = Antes_PV2; if(Soma_superpreanterior<Soma_preanterior){Antes_proximidade_val = Soma_superpreanterior;}else{Antes_proximidade_val = Soma_preanterior;};
-                }
-            }else{
-                Soma_superpreanterior = Soma_preanterior;
-                Soma_preanterior = Soma_anterior;
-                Soma_anterior = Soma_val;
+            if (minuto == 1) {
+                termo_atual = v1;
+            } else if (minuto == 2) {
+                termo_atual = v2;
+            } else {
+                termo_atual = f_penultimo + f_ultimo;
+                f_penultimo = f_ultimo;
+                f_ultimo = termo_atual;
             }
-            Antes_PV1 = Proximidade_V1; Antes_PV2 = Proximidade_V2;
 
+            // C?lculo da dist?ncia (valor absoluto manual)
+            long long dist_atual = termo_atual - num_xupenio;
+            if (dist_atual < 0) dist_atual = -dist_atual;
+
+            // REGRA 1: Melhor termo dentro do canal
+            // Se for o primeiro minuto OU dist?ncia menor OU dist?ncia igual com valor menor
+            if (canal_melhor_minuto == -1 || dist_atual < canal_melhor_dist || 
+               (dist_atual == canal_melhor_dist && termo_atual < canal_melhor_valor)) {
+                
+                canal_melhor_dist = dist_atual;
+                canal_melhor_minuto = minuto;
+                canal_melhor_valor = termo_atual;
+            }
+
+            // Condi??o de parada: se o termo j? passou muito do alvo, a dist?ncia s? vai aumentar
+            if (termo_atual > num_xupenio && termo_atual > v1 && termo_atual > v2) {
+                // Se a dist?ncia atual j? ? maior que a melhor que achamos, pode parar
+                if (dist_atual > canal_melhor_dist) break;
+            }
+            
+            // Seguran?a para evitar loops infinitos em casos de 0 0
+            if (minuto > 100) break;
         }
 
-        if(i>1 && (Antes_proximidade_canal <= Proximidade_canal)){
-            Proximidade_canal = Antes_proximidade_canal;
-            Proximidade_val = Antes_proximidade_val;
-            Min_importante = Antes_min_importante;
-            Canal_importante = i;
-        }else if(i==1){
-            Proximidade_canal = Antes_proximidade_canal;
-            Proximidade_val = Antes_proximidade_val;
-            Min_importante = Antes_min_importante;
-            Canal_importante = i;
+        // REGRA 2: Compara??o entre canais diferentes
+        // "Se dist?ncias iguais, pegue o canal de ?ndice maior"
+        // Como o loop 'i' caminha do 1 ao N, o '>=' ou '<=' garante o maior ?ndice
+        if (melhor_canal == -1 || canal_melhor_dist < melhor_distancia || 
+           (canal_melhor_dist == melhor_distancia)) {
+            
+            melhor_distancia = canal_melhor_dist;
+            melhor_canal = i;
+            melhor_minuto = canal_melhor_minuto;
+            melhor_valor_sequencia = canal_melhor_valor;
         }
-        Stop_condition = 1;
     }
 
-    long long int Valor_atual = 0;
-
-    while(Proximidade_val != 0){
-        Valor_atual+= (Proximidade_val%10);
-        Proximidade_val = Proximidade_val/10;
-    }
-    if(Valor_atual>10){
-        VIP = 1;
+    // C?lculo da soma dos algarismos do campe?o para o VIP
+    long long temp_valor = melhor_valor_sequencia;
+    int soma_digitos = 0;
+    while (temp_valor > 0) {
+        soma_digitos += (temp_valor % 10);
+        temp_valor /= 10;
     }
 
-    if(VIP == 1){
-        printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %lld e sera chamado mais ou menos no minuto %lld e com o VIP garantido!!!\n", Canal_importante, Min_importante);
-    }else if (VIP == 0){
-        printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %lld e sera chamado mais ou menos no minuto %lld, mas o ingresso VIP não vai rolar :(\n", Canal_importante, Min_importante);
+    // Sa?da final formatada
+    if (soma_digitos > 10) {
+        printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %lld e sera chamado mais ou menos no minuto %lld e com o VIP garantido!!!\n", melhor_canal, melhor_minuto);
+    } else {
+        printf("Xupenio, para ir ao lulupalooza vc deve entrar no canal %lld e sera chamado mais ou menos no minuto %lld, mas o ingresso VIP n�o vai rolar :(\n", melhor_canal, melhor_minuto);
     }
 
     return 0;
 }
+
+// O C?DIGO FOI CORRIGIDO POR IA
--- Atividades_Listas/Lista_2/at_4.c
+++ Atividades_Listas/Lista_2/at_4.c
@@ -1,56 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <math.h>
 
-int main(void){
+    int main(void){
 
-    int Q, NUM, Espaco_esq = 0, Ponto_atual = 1, Ponto_previo = 1; char W, ante_W = 'a';
+        long long int Q, NUM, Espaco_esq = 0, Ponto_atual = 1, Ponto_previo = 1; char W, ante_W = 'a';
 
-    scanf("%d", &Q);
-    for(int i = 1; i<=Q; i++){
-        scanf("%d %c", &NUM, &W);
+        scanf("%lld", &Q);
+        for(long long int i = 1; i<=Q; i++){
+            scanf("%lld %c", &NUM, &W);
 
-        if(W == 'E'){
-            if(Ponto_atual-NUM<1){
-                printf("\nInformacao invalida\n");
-                break;
-            }else if(Ponto_atual-NUM>=1){
-                Ponto_previo = Ponto_atual;
-                Ponto_atual-=NUM;
+            if(W == 'E'){
+                if(Ponto_atual-NUM<2){
+                    if(ante_W == 'E' || ante_W == 'D'){printf("\n");}
+                    printf("Informacao invalida");
+                    break;
+                }else if(Ponto_atual-NUM>=2){
+                    Ponto_previo = Ponto_atual;
+                    Ponto_atual-=NUM;
+                    if(ante_W == 'E' || ante_W == 'D'){Ponto_atual+=1;}
+                    if(ante_W == 'D' || ante_W == 'E'){printf("\n");}
+                    for(long long int x = 1; x<Ponto_atual; x++){
+                        printf(" ");
+                    }
+                    for(long long int x = Ponto_atual; x<Ponto_previo+1; x++){
+                        printf(".");
+                    }
+                }
+            }else if(W == 'D'){
                 if(ante_W == 'D' || ante_W == 'E'){printf("\n");}
-                for(int x = 1; x<Ponto_atual; x++){
+                Ponto_previo = Ponto_atual;
+                Ponto_atual+= NUM;
+                if(ante_W == 'D'){Ponto_atual+=1;}
+                if(ante_W == 'E'){Ponto_atual-=1;}
+                if(ante_W == 'B' || ante_W == 'E'){Ponto_atual+=1;}
+                for(long long int x = 1; x<Ponto_previo; x++){
                     printf(" ");
                 }
-                for(int x = Ponto_atual; x<Ponto_previo+1; x++){
+                for(long long int x = Ponto_previo; x<Ponto_atual; x++){
                     printf(".");
                 }
-            }
-        }else if(W == 'D'){
-            if(ante_W == 'D' || ante_W == 'E'){printf("\n");}
-            Ponto_previo = Ponto_atual;
-            Ponto_atual+= NUM;
-            if(ante_W == 'B' || ante_W == 'E'){Ponto_atual+=1;}
-            for(int x = 1; x<Ponto_previo; x++){
-                printf(" ");
-            }
-            for(int x = Ponto_previo; x<Ponto_atual; x++){
-                printf(".");
-            }
-            Ponto_atual-=1;
-        }else if(W == 'B'){
-            if(ante_W != 'D' && ante_W != 'E'){printf(".");}
-            if(ante_W == 'B'){NUM-=1;}
-            for(int x = 1; x<NUM; x++){
-                if(ante_W == 'E' || ante_W == 'D' || ante_W == 'B'){printf("\n");}
-                for(int x = 1; x<Ponto_atual; x++){
-                    printf(" ");
+                Ponto_atual-=1;
+            }else if(W == 'B'){
+                if(ante_W == 'B'){NUM-=1; printf(".");}
+                for(long long int x = 1; x<NUM; x++){
+                    printf("\n");
+                    for(long long int x = 1; x<Ponto_atual; x++){
+                        printf(" ");
+                    }
+                    printf(".");
+                    ante_W = W;
                 }
-                printf(".");
+                printf("\n");
             }
-            printf("\n");
+            ante_W = W;
         }
-        ante_W = W;
-    }
 
-    return 0;
-}
+        if(ante_W == 'B'){
+        for(int x = 1; x<Ponto_atual; x++){
+            printf(" ");
+        }
+        printf(".");
+        }
+        
+        return 0;
+    }
