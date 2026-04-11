19d700fa2988f7e570c3a7d4836d2f355ce20e08
mhrs-cin <mhrs@cin.ufpe.br>
2026-04-11 13:42:44 -0300

    At_1 l_5 terminada!

--- Atividades_Listas/Lista_5/at_1.c
+++ Atividades_Listas/Lista_5/at_1.c
@@ -3,73 +3,124 @@
 #include <math.h>
 #include <string.h>
 
+void wordMerge(int Merged_wsize, int Word_amount, int *Indexes, int *WORDS_size, char *Merged_word, char *WORDS[]){
 
-void printWords(char **WORDS, int Word_amount){ // Pra debug
+    int CTRL = 0, CTRL2 = 1; // CTRL = variável para controlar quando o índice de todos são iguais; CTRL2 = variável que subtrai 1 pra cada indice final
 
-    for(int i = 0; i<Word_amount; i++){
+    for(int i = 0; i<Merged_wsize-1; i++){ // for loop que itera em cada indice da merged word
+    
+        int cont = 0;
+        
+        while(1){ // loop que escolhe a palavra a ser retirada o char
 
-        printf("%s\n", WORDS[i]);
+            if((cont<Word_amount-1)&&(Indexes[cont] >= Indexes[cont+1])&&(Indexes[cont] != CTRL)){
+                cont++;
+            }else if(Indexes[cont]>=WORDS_size[cont]){
+                cont++;
+            }else{
+                break;
+            }
 
+        }
+
+        Merged_word[i] = WORDS[cont][Indexes[cont]]; // Define o char atual
+
+        Indexes[cont]++;
+        if(cont >= Word_amount-CTRL2){ // Detecta se esta na palavra final e aumenta a CTRL, se a final acabou, aumenta tambem a CTRL2
+            CTRL++;
+            if(Indexes[cont]>=WORDS_size[cont]){
+                CTRL2+=1;
+            }
+        }
     }
-    printf("\n");
 
 }
 
+void wordFusion(char *Fusion_form, int Merged_wsize, char *Merged_word){
+
+    int Fusion_pos = 0, END = 1, Fusion_amount = 0;
+
+    while(END){ // Loop que itera na Merged_word até ficar na forma certa
+
+        END = 0; Fusion_pos = 0; Fusion_amount = 0;
+
+        for(int i = 0; i<Merged_wsize-1; i++){ // for loop em si que itera
+
+            if((i<Merged_wsize-1)&&(Merged_word[i] == Merged_word[i+1])){
+
+                if(Merged_word[i]>=97 && Merged_word[i]<122){Fusion_form[Fusion_pos] = Merged_word[i] + 1;}else{Fusion_form[Fusion_pos] = 'a';}
+                if((i>0)&&(Fusion_form[Fusion_pos]==Fusion_form[Fusion_pos-1])){
+                    END = 1;
+                    Fusion_amount++;
+                }
+                i++;
+                Fusion_pos++;
+
+            }else{
+
+                Fusion_form[Fusion_pos] = Merged_word[i];
+                if((i>0)&&(Fusion_form[Fusion_pos]==Fusion_form[Fusion_pos-1])){
+                    END = 1;
+                    Fusion_amount++;
+                }
+                Fusion_pos++;
+
+            }
+
+        }
+
+        strcpy(Merged_word, Fusion_form);
+        if(END == 1){
+            for(int i = 0; i<Fusion_pos; i++){
+                Fusion_form[i] = '\0';
+            }
+        }
+        Merged_wsize = Fusion_pos+1;
+
+    }
+}
 
 int main(void){
 
     int Word_amount, Merged_wsize = 1; scanf("%d", &Word_amount);
 
-    char **WORDS = calloc(Word_amount, sizeof(*WORDS)), *Merged_word = calloc(Merged_wsize, sizeof(*Merged_word)); 
+    char *WORDS[Word_amount], *Merged_word = calloc(Merged_wsize, sizeof(*Merged_word)), *Fusion_form = calloc(Merged_wsize, sizeof(*Fusion_form)); 
     
     int *WORDS_size = calloc(Word_amount, sizeof(*WORDS_size)), *Indexes = calloc(Word_amount, sizeof(*Indexes));
 
-    if((WORDS != NULL)&&(Merged_word != NULL)&&(WORDS_size != NULL)&&(Indexes != NULL)){
+    if((Merged_word != NULL)&&(WORDS_size != NULL)&&(Indexes != NULL)&&(Fusion_form != NULL)){
 
         for(int i = 0; i<Word_amount; i++){
 
-            WORDS[i] = realloc(WORDS[i], 100);
-            scanf("%s", WORDS[i]);
-            WORDS_size[i] = strlen(WORDS[i]);
-            WORDS[i] = realloc(WORDS[i], WORDS_size[i]);
-            Merged_wsize += WORDS_size[i];
+            WORDS[i] = calloc(200, sizeof(*WORDS[i]));
+            if(WORDS[i] != NULL){
+                scanf("%s", WORDS[i]);
+                WORDS_size[i] = strlen(WORDS[i]);
+                WORDS[i] = realloc(WORDS[i], WORDS_size[i]+1);
+                Merged_wsize += WORDS_size[i];
+            }else{
+                printf("Problema de memória");
+                exit(1);
+            }
 
         }
 
-        Merged_word = realloc(Merged_word, Merged_wsize);
+        Merged_word = realloc(Merged_word, Merged_wsize); Fusion_form = realloc(Fusion_form, Merged_wsize); // realoca pro tamanho certo
 
-        int CTRL = 0;
+        wordMerge(Merged_wsize, Word_amount, Indexes, WORDS_size, Merged_word, WORDS); // Função que da merge nas palavras
 
-        for(int i = 0; i<Merged_wsize-1; i++){
+        char True_merged_word[Merged_wsize]; strcpy(True_merged_word, Merged_word); // Merged word verdadeira
+
+        wordFusion(Fusion_form, Merged_wsize, Merged_word); // Função que funde os chars
         
-            int cont = 0;
-            
-            while(1){
+        printf("%s\n", True_merged_word);
+        printf("%s\n", Fusion_form);
 
-                if((cont<Word_amount)&&(Indexes[cont] >= Indexes[cont+1])&&(Indexes[cont] != CTRL)){
-                    cont++;
-                }else if(Indexes[cont]>=WORDS_size[cont]){
-                    cont++;
-                }else{
-                    break;
-                }
-
-            }
-
-            if(cont >= Word_amount-1){
-                CTRL++;
-            }
-
-            Merged_word[i] = WORDS[cont][Indexes[cont]];
-
-            Indexes[cont]++;
-
+        free(Merged_word); free(WORDS_size); free(Indexes); free(Fusion_form);
+        for(int i = 0; i<Word_amount; i++){
+            free(WORDS[i]);
         }
 
-        printWords(WORDS, Word_amount);
-        printf("%s\n", Merged_word);
-
-        free(WORDS); free(Merged_word); free(WORDS_size);
     }else{
         printf("Erro de alocação.");
         exit(1);
