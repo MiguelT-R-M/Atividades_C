1b566d45a3a06c32c735aa1a4a454600269e4795
MiguelT-R-M <miguelhrs123@gmail.com>
2026-04-12 13:35:36 -0300

    at_3 terminada...!

---
+++ Atividades_Listas/Lista_5/at_2.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+int notasManipulation(int *Alunos_quant, float *NOTAS, float *MEDIA, float *Maior_nota, float *Menor_nota, int *Acima_media, float *MEDIANA, int *Moda_pos, int Novos_alunos_bool, int *Maior_nota_pos, int *Menor_nota_pos, int Novos_alunos, float *Notas_org){
+
+    if(Novos_alunos_bool == 0){
+        for(int i = (*Alunos_quant-Novos_alunos)*(Novos_alunos_bool); i<*Alunos_quant; i++){
+
+            float TEMPFLOAT; scanf("%f", &TEMPFLOAT);
+            NOTAS[i] =  TEMPFLOAT;
+
+        }
+
+        for(int i = 0; i<*Alunos_quant; i++){
+            *MEDIA+=NOTAS[i];
+        }
+        *MEDIA = *MEDIA/(*Alunos_quant);
+        for(int i = 0; i<*Alunos_quant; i++){
+            if(NOTAS[i]>*MEDIA){
+                *Acima_media+=1;
+            }
+            for(int j = 1+i; j<*Alunos_quant; j++){
+                if((NOTAS[j]>NOTAS[i])&&(NOTAS[j]>NOTAS[(int)*Maior_nota])){
+                    *Maior_nota = j;
+                }
+                if(NOTAS[j]<NOTAS[i]&&(NOTAS[j]<NOTAS[(int)*Menor_nota])){
+                    *Menor_nota = j;
+                }
+            }
+        }
+    }else{
+
+        *Maior_nota = 0; *Menor_nota = 0; *MEDIA = 0; *Acima_media = 0;
+
+        for(int i = (*Alunos_quant-Novos_alunos)*(Novos_alunos_bool); i<*Alunos_quant; i++){
+
+            float TEMPFLOAT; scanf("%f", &TEMPFLOAT);
+            NOTAS[i] =  TEMPFLOAT;
+
+        }
+
+        for(int i = 0; i<*Alunos_quant; i++){
+            *MEDIA+=NOTAS[i];
+        }
+        *MEDIA = *MEDIA/(*Alunos_quant);
+        for(int i = 0; i<*Alunos_quant; i++){
+            if(NOTAS[i]>*MEDIA){
+                *Acima_media+=1;
+            }
+            for(int j = 1+i; j<*Alunos_quant; j++){
+                if((NOTAS[j]>NOTAS[i])&&(NOTAS[j]>NOTAS[(int)*Maior_nota])){
+                    *Maior_nota = j;
+                }
+                if(NOTAS[j]<NOTAS[i]&&(NOTAS[j]<NOTAS[(int)*Menor_nota])){
+                    *Menor_nota = j;
+                }
+            }
+        }
+    }
+    *Maior_nota_pos = (int)*Maior_nota;
+    *Menor_nota_pos = (int)*Menor_nota;
+    *Maior_nota = NOTAS[(int)*Maior_nota];
+    *Menor_nota = NOTAS[(int)*Menor_nota];
+
+    for(int i = 0; i<*Alunos_quant; i++){
+        Notas_org[i] = NOTAS[i];
+    }
+
+    for(int i = 0; i<*Alunos_quant; i++){
+        for(int j = 1+i; j<*Alunos_quant; j++){
+            if(Notas_org[j]<Notas_org[i]){
+                float TEMPFLOAT = Notas_org[i];
+                Notas_org[i] = Notas_org[j];
+                Notas_org[j] = TEMPFLOAT;
+            }
+        }
+    }
+
+    if(*Alunos_quant%2==0){
+        *MEDIANA = ((Notas_org[(*Alunos_quant-1)/2])+(Notas_org[((*Alunos_quant-1)/2)+1]))/2;
+    }else{
+        *MEDIANA = Notas_org[(*Alunos_quant/2)];
+    }
+
+    int Repeat_quant = 0, Repeat_max = 0;
+    for(int i = 0; i<*Alunos_quant; i++){
+        if((i<*Alunos_quant-1)&&(Notas_org[i]==Notas_org[i+1])){
+            Repeat_quant++;
+        }else{
+            if(Repeat_quant>Repeat_max){ Repeat_max = Repeat_quant; *Moda_pos = i;}else if(Repeat_quant == Repeat_max){ *Moda_pos = -1;}
+            Repeat_quant = 0;
+        }
+    }
+
+}
+
+int main(void){
+
+    int Alunos_quant = 0, Novos_alunos_bool = 0, Novos_alunos = 0, Acima_media = 0, Moda_pos = 0, Maior_nota_pos = 0, Menor_nota_pos = 0; scanf("%d", &Alunos_quant);
+
+    float *NOTAS = calloc(Alunos_quant, sizeof(*NOTAS)), *Notas_org = calloc(Alunos_quant, sizeof(*Notas_org)),MEDIA = 0.0, Maior_nota = 0, Menor_nota = 0, MEDIANA;
+
+    int *Alunos_quant_pont = &Alunos_quant, *Acima_media_pont = &Acima_media, *Moda_pos_pont = &Moda_pos, *Maior_nota_pos_pont = &Maior_nota_pos, *Menor_nota_pos_pont = &Menor_nota_pos;
+    float *MEDIA_pont = &MEDIA, *Maior_nota_pont = &Maior_nota, *Menor_nota_pont = &Menor_nota, *MEDIANA_pont = &MEDIANA;
+
+    if(NOTAS != NULL){
+
+
+        notasManipulation(Alunos_quant_pont, NOTAS, MEDIA_pont, Maior_nota_pont, Menor_nota_pont, Acima_media_pont, MEDIANA_pont, Moda_pos_pont, Novos_alunos_bool, Maior_nota_pos_pont, Menor_nota_pos_pont, Novos_alunos, Notas_org);
+
+        printf("Relatorio inicial\n");
+
+        if(Moda_pos > 0){
+            printf("Media: %.2f\nMaior nota: %.2f (aluno %d)\nMenor nota: %.2f (aluno %d)\nAcima da media: %d\nMediana: %.2f\nModa: %.2f\n\n", MEDIA, Maior_nota, Maior_nota_pos+1, Menor_nota, Menor_nota_pos+1, Acima_media, MEDIANA, Notas_org[Moda_pos]);
+        }else{
+            printf("Media: %.2f\nMaior nota: %.2f (aluno %d)\nMenor nota: %.2f (aluno %d)\nAcima da media: %d\nMediana: %.2f\nModa: Nao ha moda unica\n\n", MEDIA, Maior_nota, Maior_nota_pos+1, Menor_nota, Menor_nota_pos+1, Acima_media, MEDIANA);
+        }
+
+        scanf("%d", &Novos_alunos);
+        Alunos_quant+=Novos_alunos;
+        Novos_alunos_bool = 1;
+        NOTAS = realloc(NOTAS, Alunos_quant);
+        Notas_org = realloc(Notas_org, Alunos_quant);
+
+        notasManipulation(Alunos_quant_pont, NOTAS, MEDIA_pont, Maior_nota_pont, Menor_nota_pont, Acima_media_pont, MEDIANA_pont, Moda_pos_pont, Novos_alunos_bool, Maior_nota_pos_pont, Menor_nota_pos_pont, Novos_alunos, Notas_org);
+
+        printf("Relatorio atualizado\n");
+        if(Moda_pos > 0){
+            printf("Media: %.2f\nMaior nota: %.2f (aluno %d)\nMenor nota: %.2f (aluno %d)\nAcima da media: %d\nMediana: %.2f\nModa: %.2f\n\n", MEDIA, Maior_nota, Maior_nota_pos+1, Menor_nota, Menor_nota_pos+1, Acima_media, MEDIANA, Notas_org[Moda_pos]);
+        }else{
+            printf("Media: %.2f\nMaior nota: %.2f (aluno %d)\nMenor nota: %.2f (aluno %d)\nAcima da media: %d\nMediana: %.2f\nModa: Nao ha moda unica\n\n", MEDIA, Maior_nota, Maior_nota_pos+1, Menor_nota, Menor_nota_pos+1, Acima_media, MEDIANA);
+        }
+
+        free(NOTAS);
+
+    }else{
+
+        printf("Alocação inválida de memória.");
+        exit(1);
+
+    }
+
+    return 0;
+}
